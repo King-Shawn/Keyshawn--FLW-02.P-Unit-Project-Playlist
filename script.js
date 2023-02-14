6222f0a57de01cca46f9e690ec433e262c0a7adc
Keyshawn <94555347+King-Shawn@users.noreply.github.com>
2023-02-14 00:30:33 +0000

    I finished task 10 and changed some css styles.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // task 5: read through the JavaScript starter code to determine where each given function is declared and where each given function is called.
 
-
 // input variables
 let image = document.querySelector(".image");
 let songName = document.querySelector(".song-name");
@@ -10,7 +9,6 @@ let songLink = document.querySelector(".song-link");
 // button variable
 let add = document.querySelector(".add");
 
-
 // task 6: declare variables for your display divs: the image url, song name, artist, and song link. Go back to the HTML to check that you are using the correct class names.
 
 let display = document.querySelector(".display");
@@ -19,13 +17,25 @@ let displayArtist = document.querySelector(".display-artist");
 let displayImage = document.querySelector(".display-image");
 let displayLink = document.querySelector(".display-link");
 
-
-
 // task 7: create and populate an array to store your image urls. Create three more arrays. One to store your song names, one for the artists, and a last one for the song links.
+let images = ["https://i1.sndcdn.com/artworks-oDdflDikYpVv-0-t500x500.png",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4KzE3pt5ti2ZVKRc7G-FgRr5pIsufTBhLZQ&usqp=CAU",
+"https://i.scdn.co/image/ab67616d0000b27308e30ab6a058429303d75876",
+"https://upload.wikimedia.org/wikipedia/en/2/2f/KSI_-_Holiday.png",
+"https://i.scdn.co/image/ab67616d0000b2732bf0876d42b90a8852ad6244",
+"https://upload.wikimedia.org/wikipedia/en/3/36/%22Chicago%22_promotional_cover.jpg"
+] ;
 
+let song = ["Heart Mind", "Golden Hour", "Intentions", "Holiday", "Until I found you (Em Beihold Version)", "Chicago"];
 
+let artists = ["Kodak Black & Plies", "Jvke", "Justin Bieber & Quavo", "KSI", "Stephen Sanchez & Em Beihold", "Michael Jackson"];
 
-
+let link = ["https://www.youtube.com/watch?v=VZJoUC1F5xI",
+"https://www.youtube.com/watch?v=UsR08cY8k0A",
+"https://www.youtube.com/watch?v=3AyMjyHu1bA",
+"https://www.youtube.com/watch?v=gfHbg53ykPY",
+"https://www.youtube.com/watch?v=kPlSyYtE63M",
+"https://www.youtube.com/watch?v=Y_8mUx4VOmo"];
 
 //REFACTOR ARRAYS DAY 
 // task 11: comment out the arrays data.
@@ -42,19 +52,20 @@ let displayLink = document.querySelector(".display-link");
 
 
 
-
-
 function addSongInfo() {
 
 // task 9: declare a variable to save the user input of the image url. Declare three more variables that save user input: One for the song names, one for the artists, and a last one for the song links.
-
+let newImg = image.value; 
+let newSong = songName.value; 
+let newArtist = artist.value; 
+let newLink = songLink.value; 
 
 // task 10: use `.push()` to add each input value to the correct array.
-
+images.push(newImg);
+song.push(newSong);
+artists.push(newArtist);
+link.push(newLink);
 }
-
-
-
 
 /******** this function empties the display divs each time the button is clicked so that your playlist does not repeatedly add the data too many times. Where should this function be placed???********/
 function emptyDisplay() {
@@ -64,20 +75,27 @@ function emptyDisplay() {
   displayLink.innerHTML = "";
 }
 
-
-
-
 function displaySongInfo() {
 
 // task 8: loop through your images array and display the images to your songs in the correct div. Create three more loops. One for the song names, one for the artists, and a last one for the song links.
+  console.log("wh got here");
+  images.forEach(function(img) {
+  displayImage.insertAdjacentHTML("Beforeend", `<p> <img src = ${img}> </p>`)
+});
 
+  song.forEach(function(songs) {
+  displaySong.insertAdjacentHTML("Beforeend", `<p>${songs}</p>`)
+});
 
+  artists.forEach(function(artist) {
+  displayArtist.insertAdjacentHTML("Beforeend", `<p>${artist}</p>`)
+});
 
+  link.forEach(function(links) {
+  displayLink.insertAdjacentHTML("Beforeend", `<p> <a href =${links} target="_blank"> This songs link<a/></p>`)
+});
+  
 }
-
-
-
-
 
 // click event to add and display songs
 add.onclick = function() {
